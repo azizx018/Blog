999a28e3b3152f85e65e529076f3feb6a45ed4e7
azizx018 <chiara.aziz@gmail.com>
2023-11-01 17:00:07 -0500

    set up everything for react router

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,15 +3,23 @@ import About from './pages/About';
 import ArticleListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <h1>BLOGS!</h1>
-     <div id="page-body">
-      Welcome to the Lair!
-     </div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <h1>BLOGS!</h1>
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/articles" element={<ArticleListPage/>} />
+          <Route path="/articles/:articleId" element={<ArticlePage/>} />
+        </Routes>
+      </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
