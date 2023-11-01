81b688ee30ac092974feb4bf362c774cddf4589e
azizx018 <chiara.aziz@gmail.com>
2023-11-01 16:52:59 -0500

    added pages comp

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,7 @@
-
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import ArticleListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 import './App.css';
 
 function App() {
