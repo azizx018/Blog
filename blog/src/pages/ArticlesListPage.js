c7e9a29addb87c1992d714727f47a558480a6a4a
azizx018 <chiara.aziz@gmail.com>
2023-11-03 11:54:35 -0500

    articles list page

--- blog/src/pages/ArticlesListPage.js
+++ blog/src/pages/ArticlesListPage.js
@@ -1,8 +1,22 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticleListPage = () => {
     return (
-        <h1>
-            This is the article list page
-        </h1>
+        <>
+        <h1>Articles</h1>
+        {/* //loop through and display all in a list */}
+        {articles.map(article => (
+            <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}...</p>
+            </Link>
+        ))}
+        
+        </>
+        
+        
+
     );
 }
 
