fda43d58705a3ff1538e669a5ff5bc40e0ce0610
azizx018 <chiara.aziz@gmail.com>
2023-11-03 12:44:56 -0500

    basic upvote endpoint with express and node

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,9 +1,32 @@
 import express from 'express';
 
-const app = express();
+let artilcesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+},
+{
+    name: 'learn-node',
+    upvotes: 0
+},
+{
+    name: 'mongodb',
+    upvotes: 0
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello there you little piglet');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const {name} = req.params;
+    const article = artilcesInfo.find(a => a.name == name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvote`);
+
+    } else {
+        res.send('That article is not found')
+    }
+    
 
 });
 
