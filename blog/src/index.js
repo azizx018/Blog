29f6616c8dc6ad95246254f35e0fdc2baa720a8b
azizx018 <chiara.aziz@gmail.com>
2023-11-07 09:53:03 -0600

    added firebase auth

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-YQwIMpSTb09fsHzY9GeHQklNMd0S0Pc",
+  authDomain: "react-blog-c1dfe.firebaseapp.com",
+  projectId: "react-blog-c1dfe",
+  storageBucket: "react-blog-c1dfe.appspot.com",
+  messagingSenderId: "607333188413",
+  appId: "1:607333188413:web:68672df87b5c2dcce77b54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +28,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
