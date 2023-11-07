6105ca93e015db3430eadee6fc0590b56b418b8d
azizx018 <chiara.aziz@gmail.com>
2023-11-07 11:13:08 -0600

    edit upvotes so user need to be logged in

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -13,29 +13,60 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e) {
+            res.sendStatus(400);
+        }
+    }
+    next();
+    
+});
+
 app.get('/api/articles/:name', async(req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db?.collection('articles').findOne({ name });
 
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
     }  
 });
 
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+
+});
+
 app.put('/api/articles/:name/upvote', async(req, res) => {
     const { name } = req.params;
-    await db.collection('articles').updateOne({ name }, {
-        //the inc increases a value in mongo
-        $inc: { upvotes: 1 },
-    });
-    const article = await db.collection('articles').findOne({ name });
+    const { uid } = req.user;
 
     if (article) {
-        res.json(article);
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.include(uid);
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                //the inc increases a value in mongo
+                $inc: { upvotes: 1 },
+                $push: {upvoteIds: uid}
+            });
 
+        }
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle);
     } else {
         res.send('That article is not found')
     }
